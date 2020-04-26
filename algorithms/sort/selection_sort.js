4de1e401e3479cca57d00124168ebe9f6083ee08
sknv <s.kanukov@gmail.com>
2020-04-26 11:30:09 +0300

    Selection sort

---
+++ algorithms/sort/selection_sort.js
@@ -0,0 +1,22 @@
+const swap = (arr, i, j) => {
+    const tmp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = tmp
+}
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const selectionSort = (nums) => {
+    for (let i = 0; i < nums.length - 1; i++) {
+        let minIndex = i
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[j] < nums[minIndex]) {
+                minIndex = j
+            }
+        }
+        swap(nums, i, minIndex)
+    }
+    return nums
+}
