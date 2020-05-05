420bfa985e7138bee53b7e5d9dc3ea3379f9489c
sknv <s.kanukov@gmail.com>
2020-05-05 10:27:56 +0300

    Bubble sort

---
+++ algorithms/sort/bubbleSort.test.ts
@@ -0,0 +1,13 @@
+import bubbleSort from './bubbleSort.ts'
+
+function testBubbleSort() {
+    let nums = [5, -2, 12, 0, 9], expected = [-2, 0, 5, 9, 12]
+    bubbleSort(nums)
+    console.assert(JSON.stringify(nums) === JSON.stringify(expected), `expect bubbleSort(...) to be: ${expected}, got instead: ${nums}`)
+
+    nums = [1, -2, 10, -6], expected = [-6, -2, 1, 10]
+    bubbleSort(nums)
+    console.assert(JSON.stringify(nums) === JSON.stringify(expected), `expect bubbleSort(...) to be: ${expected}, got instead: ${nums}`)
+}
+
+testBubbleSort()
