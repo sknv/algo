3f5e417acb05050d9ea818a246038447ab5b2452
sknv <s.kanukov@gmail.com>
2020-05-06 11:13:28 +0300

    Fix test messages

--- data-structures/lru-cache/LRUCache.test.ts
+++ data-structures/lru-cache/LRUCache.test.ts
@@ -10,11 +10,11 @@ function testLRUCache() {
 
     cache.put(3, 3) // evicts key 2
     value = cache.get(2)
-    console.assert(!value, 'expect cached value to be equal blank')
+    console.assert(!value, 'expect cached value to be blank')
 
     cache.put(4, 4) // evicts key 1
     value = cache.get(1)
-    console.assert(!value, 'expect cached value to be equal blank')
+    console.assert(!value, 'expect cached value to be blank')
 
     value = cache.get(3)
     console.assert(value === 3, 'expect cached value to be equal 3')
