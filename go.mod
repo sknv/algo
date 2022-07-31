77de7f9f4e2a09922d9ad9576b644c52b020b6e9
sknv <s.kanukov@gmail.com>
2022-07-31 12:43:14 +0300

    Merge go channels using generics

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module algo
 
-go 1.15
+go 1.18
---
+++ golang/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	runMergeChannels()
+}
+
+func runMergeChannels() {
+	a := make(chan int)
+	b := make(chan int)
+	c := make(chan int)
+
+	go func() {
+		for _, num := range []int{1, 2, 3} {
+			a <- num
+		}
+		close(a)
+	}()
+
+	go func() {
+		for _, num := range []int{20, 10, 30} {
+			b <- num
+		}
+		close(b)
+	}()
+
+	go func() {
+		for _, num := range []int{300, 200, 100} {
+			c <- num
+		}
+		close(c)
+	}()
+
+	for num := range MergeChannels(a, b, c) {
+		fmt.Println(num)
+	}
+}
