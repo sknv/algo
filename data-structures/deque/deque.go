e03331eddd755538df01e2833594413fa850ea9e
Sergey Kanukov <s.kanukov@gmail.com>
2024-08-07 18:58:08 +0300

    Add a simple deque

---
+++ data-structures/deque/deque.go
@@ -0,0 +1,49 @@
+package deque
+
+type Deque[T any] struct {
+	data []T
+}
+
+func NewDeque[T any](capacity int) *Deque[T] {
+	return &Deque[T]{
+		data: make([]T, 0, capacity),
+	}
+}
+
+// PushFront adds an element to the front of the deque.
+func (d *Deque[T]) PushFront(value T) {
+	d.data = append([]T{value}, d.data...)
+}
+
+// PushBack adds an element to the back of the deque.
+func (d *Deque[T]) PushBack(value T) {
+	d.data = append(d.data, value)
+}
+
+// PopFront removes and returns an element from the front of the deque.
+func (d *Deque[T]) PopFront() (T, bool) {
+	if len(d.data) == 0 {
+		var zero T
+
+		return zero, false
+	}
+
+	value := d.data[0]
+	d.data = d.data[1:]
+
+	return value, true
+}
+
+// PopBack removes and returns an element from the back of the deque.
+func (d *Deque[T]) PopBack() (T, bool) {
+	if len(d.data) == 0 {
+		var zero T
+
+		return zero, false
+	}
+
+	value := d.data[len(d.data)-1]
+	d.data = d.data[:len(d.data)-1]
+
+	return value, true
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module algo
 
-go 1.18
+go 1.22
